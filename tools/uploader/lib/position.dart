7dbc6481ae8ddb04dc4014b3880b6883fdb54e43
Pietro Panizza <pietropanizza88@gmail.com>
2025-08-24 11:41:07 +0200

    Fix distance computation.

--- tools/uploader/lib/position.dart
+++ tools/uploader/lib/position.dart
@@ -8,11 +8,10 @@ class Position {
 
   double getDistanceTo(Position other) {
     const earthRadius = 6371e3; // meters
-    final lat1 = latitude * (3.141592653589793238 / 180);
-    final lat2 = other.latitude * (3.141592653589793238 / 180);
-    final deltaLat = (other.latitude - latitude) * (3.141592653589793238 / 180);
-    final deltaLon =
-        (other.longitude - longitude) * (3.141592653589793238 / 180);
+    final lat1 = latitude * (pi / 180);
+    final lat2 = other.latitude * (pi / 180);
+    final deltaLat = (other.latitude - latitude) * (pi / 180);
+    final deltaLon = (other.longitude - longitude) * (pi / 180);
     final a =
         (sin(deltaLat / 2) * sin(deltaLat / 2)) +
         (cos(lat1) * cos(lat2) * sin(deltaLon / 2) * sin(deltaLon / 2));
