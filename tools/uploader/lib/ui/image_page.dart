1421c99867d05610f2cafb5695f40617fd43f64a
Pietro Panizza <pietropanizza88@gmail.com>
2025-08-31 11:19:37 +0200

    Add status to image path.

--- tools/uploader/lib/ui/image_page.dart
+++ tools/uploader/lib/ui/image_page.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
 import 'package:provider/provider.dart';
 
 import '../model/image_manager.dart';
@@ -60,74 +61,94 @@ class _ImagePageState extends State<ImagePage> {
                     ],
                   ),
                 )
-              : GridView.builder(
+              : ListView(
                   padding: const EdgeInsets.all(40),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 5,
-                    childAspectRatio: 1,
-                    crossAxisSpacing: 20,
-                    mainAxisSpacing: 20,
-                  ),
-                  itemCount: manager.images.length,
-                  itemBuilder: (context, index) {
-                    final img = manager.images[index];
-                    return ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Stack(
-                        fit: StackFit.expand,
-                        children: [
-                          Image.file(
-                            img.file,
-                            fit: BoxFit.cover,
-                            frameBuilder:
-                                (
-                                  context,
-                                  child,
-                                  frame,
-                                  wasSynchronouslyLoaded,
-                                ) {
-                                  if (wasSynchronouslyLoaded || frame != null) {
-                                    return child;
-                                  }
-                                  return Container(
-                                    color: Colors.white.withAlpha(10),
-                                  );
-                                },
+                  children: [
+                    GridView.builder(
+                      physics:
+                          NeverScrollableScrollPhysics(), // to disable GridView's scrolling
+                      shrinkWrap: true,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 5,
+                            childAspectRatio: 1,
+                            crossAxisSpacing: 20,
+                            mainAxisSpacing: 20,
                           ),
-                          Positioned(
-                            left: 0,
-                            right: 0,
-                            bottom: 0,
-                            height: 30,
-                            child: Container(
-                              color: img.position != null
-                                  ? Colors.green.withAlpha(180)
-                                  : Colors.red.withAlpha(180),
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(
-                                  horizontal: 20,
-                                ),
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: [
-                                    Tooltip(
-                                      message: img.file.path,
-                                      child: Icon(Icons.image, size: 16),
+                      itemCount: manager.images.length,
+                      itemBuilder: (context, index) {
+                        final img = manager.images[index];
+                        return ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Stack(
+                            fit: StackFit.expand,
+                            children: [
+                              Image.file(
+                                img.file,
+                                fit: BoxFit.cover,
+                                frameBuilder:
+                                    (
+                                      context,
+                                      child,
+                                      frame,
+                                      wasSynchronouslyLoaded,
+                                    ) {
+                                      if (wasSynchronouslyLoaded ||
+                                          frame != null) {
+                                        return child;
+                                      }
+                                      return Container(
+                                        color: Colors.white.withAlpha(10),
+                                      );
+                                    },
+                              ),
+                              Positioned(
+                                left: 0,
+                                right: 0,
+                                bottom: 0,
+                                height: 30,
+                                child: Container(
+                                  color: img.position != null
+                                      ? Colors.green.withAlpha(180)
+                                      : Colors.red.withAlpha(180),
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                      horizontal: 20,
                                     ),
-                                    const SizedBox(width: 10),
-                                    Tooltip(
-                                      message: img.position?.toString() ?? '',
-                                      child: Icon(Icons.location_on, size: 16),
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: [
+                                        Tooltip(
+                                          message: basename(img.file.path),
+                                          child: Icon(Icons.image, size: 16),
+                                        ),
+
+                                        if (img.position != null) ...[
+                                          const SizedBox(width: 10),
+                                          Tooltip(
+                                            message: img.position!.toString(),
+                                            child: Icon(
+                                              Icons.location_on,
+                                              size: 16,
+                                            ),
+                                          ),
+                                        ],
+                                      ],
                                     ),
-                                  ],
+                                  ),
                                 ),
                               ),
-                            ),
+                            ],
                           ),
-                        ],
-                      ),
-                    );
-                  },
+                        );
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    Text('Totale immagini: ${manager.images.length}'),
+                    Text(
+                      'Immagini senza posizione: ${manager.images.where((img) => img.position == null).length}',
+                    ),
+                  ],
                 ),
         );
       },
